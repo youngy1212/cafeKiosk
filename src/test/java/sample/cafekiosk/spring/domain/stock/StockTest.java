47936d75d53e1103ae35ab776901b9252595868e
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2024-12-02 21:11:40 +0900

    @Dynamic Test

--- src/test/java/sample/cafekiosk/spring/domain/stock/StockTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/stock/StockTest.java
@@ -3,8 +3,12 @@ package sample.cafekiosk.spring.domain.stock;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Collection;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 
 class StockTest {
 
@@ -50,6 +54,44 @@ class StockTest {
                  .isInstanceOf(IllegalArgumentException.class)
                  .hasMessage("차감할 재고 수량이 없습니다.");
 
+    }
+
+    @DisplayName("")
+    @TestFactory
+    Collection<DynamicTest> dynamicTests(){
+        return List.of(
+                DynamicTest.dynamicTest("", ()->{
+
+                }),
+                DynamicTest.dynamicTest("",()->{
+
+                })
+        );
+    }
+
+    @DisplayName("재고 차감 시나리오")
+    @TestFactory
+    Collection<DynamicTest> stockDeductionDynamicTest() {
+        // given
+        Stock stock = Stock.create("001", 1);
+        return List.of(
+                DynamicTest.dynamicTest("재고를 주어진 개수만큼 차감할 수 있다.", () -> {
+                    // given
+                    int quantity = 1;
+                    // when
+                    stock.deductQuantity(quantity); //차감
+                    // then
+                    assertThat(stock.getQuantity()).isZero(); //0이됬는지 확인
+                }),
+                DynamicTest.dynamicTest("재고보다 많은 수의 수량으로 차감 시도하는 경우 예외가 발생한다.", () -> {
+                    // given
+                    int quantity = 1;
+                    // when // then
+                    assertThatThrownBy(() -> stock.deductQuantity(quantity))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessage("차감할 재고 수량이 없습니다.");
+                })
+        );
     }
 
 }
